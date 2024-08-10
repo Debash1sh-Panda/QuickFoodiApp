671fbd79b4f3209afff44074eae78f5e39ec0ed0
Debash1sh-Panda <debasishpanda2036@gmail.com>
2024-08-10 18:52:41 +0530

    edit

--- quickfoodi-client/src/pages/shop/Cart.jsx
+++ quickfoodi-client/src/pages/shop/Cart.jsx
@@ -103,132 +103,147 @@ function Cart() {
 
   return (
     <div className="section-container bg-gradient-to-r from-[#164A41] from-0% via-[#4D774E] via-40% to-[#9DCBBD] to-100%">
-      {/* text */}
-      <div className="py-32 flex flex-col justify-between items-center gap-8">
-        <div className="flex items-center justify-center space-y-7 px-3 order-2 md:order-1">
-          <h2 className="md:text-5xl text-4xl font-bold md:leading-snug leading-snug">
-            Items Added to The{" "}
-            <span className="text-[#164A41] font-bold">CART</span>
+      {cart.length === 0 ? (
+        <div className="flex flex-col justify-center items-center py-32">
+          <h2 className="text-4xl md:text-5xl font-bold text-white mb-8">
+            Your Cart is Empty
           </h2>
-        </div>
-      </div>
-
-      {/* table */}
-      <div className="overflow-x-auto">
-        <table className="table">
-          {/* head */}
-          <thead className="bg-[#F1B24A] text-black rounded-md">
-            <tr>
-              <th>#</th>
-              <th>Food</th>
-              <th>Item Name</th>
-              <th>Quantity</th>
-              <th>Price</th>
-              <th>Action</th>
-            </tr>
-          </thead>
-          <tbody>
-            {/* row 1 */}
-            {cart.map((item, index) => (
-              <tr key={index}>
-                <td>{index + 1}</td>
-                <td>
-                  <div className="flex items-center gap-3">
-                    <div className="avatar">
-                      <div className="mask mask-squircle h-12 w-12">
-                        <img
-                          src={item.image}
-                          alt="Avatar Tailwind CSS Component"
-                        />
-                      </div>
-                    </div>
-                  </div>
-                </td>
-                <td>{item.name}</td>
-                <td>
-                  <button
-                    className="btn btn-xs text-red-500"
-                    onClick={() => handleDecrease(item)}
-                  >
-                    -
-                  </button>
-                  <input
-                    type="number"
-                    value={item.quantity}
-                    onChange={() => console.log(item.quantity)}
-                    className="w-10 mx-2 text-center overflow-hidden appearance-none text-[#dccf1a]"
-                  />
-                  <button
-                    className="btn btn-xs text-green-500"
-                    onClick={() => handleIncrease(item)}
-                  >
-                    +
-                  </button>
-                </td>
-                <td className="text-black">₹{totalPrice(item).toFixed(2)}</td>
-                <th>
-                  <button
-                    className="btn btn-ghost btn-xs text-black hover:text-red-800 hover:bg-slate-500"
-                    onClick={() => handleDelete(item)}
-                  >
-                    <FaTrash />
-                  </button>
-                </th>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      </div>
-
-      {/* customer details */}
-      <div className="my-12 flex flex-col md:flex-row">
-        <div className="md:w-1/2 space-y-3 relative">
-          <h3 className="text-[#dccf1a] font-bold">Customer Details</h3>
-          <label
-            htmlFor="my-drawer-4"
-            className="drawer-button btn btn-ghost btn-circle avatar"
-          >
-            <div className="w-10 rounded-full">
-              {user.photoURL ? (
-                <img alt="User avatar" src={user.photoURL} />
-              ) : (
-                <img
-                  alt="Default avatar"
-                  src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg"
-                />
-              )}
-            </div>
-          </label>
-          <p className="absolute top-9 px-14">
-            Name: <span className="text-black">{user.displayName}</span>
-          </p>
-          <p>
-            Email: <span className="text-black">{user.email}</span>
-          </p>
-          <p>
-            User_id: <span className="text-black">{user.uid}</span>
-          </p>
-        </div>
-
-        <div className="md:w-1/2 space-y-3 flex flex-col justify-end  items-end">
-          <h3 className="text-[#123a34] font-bold">Shopping Details</h3>
-          <p className=" text-black">
-            Total Items:{" "}
-            <span className="text-black font-bold">{cart.length}</span>
-          </p>
-          <p className="text-black">
-            Total Price:{" "}
-            <span className="text-[#102929] font-bold">
-              ₹{calculateTotalAmount.toFixed(2)}
-            </span>
-          </p>
-          <Link to='/process-checkout'>
-            <button className="btn bg-[#F1B24A] hover:bg-[#f0c684] hover:text-slate-800 rounded-full px-6 text-white flex items-center gap-2 mt-4 mb-5">
-              Proceed Checkout
+          <Link to="/menu">
+            <button className=" mt-14 mb-60 btn bg-[#F1B24A] hover:bg-[#f0c684] hover:text-slate-800 rounded-full px-6 text-white">
+              Continue Shopping
             </button>
           </Link>
         </div>
-      </div>
+      ) : (
+        <>
+          {/* text */}
+          <div className="py-32 flex flex-col justify-between items-center gap-8">
+            <div className="flex items-center justify-center space-y-7 px-3 order-2 md:order-1">
+              <h2 className="md:text-5xl text-4xl font-bold md:leading-snug leading-snug">
+                Items Added to The{" "}
+                <span className="text-[#164A41] font-bold">CART</span>
+              </h2>
+            </div>
+          </div>
+
+          {/* table */}
+          <div className="overflow-x-auto">
+            <table className="table">
+              {/* head */}
+              <thead className="bg-[#F1B24A] text-black rounded-md">
+                <tr>
+                  <th>#</th>
+                  <th>Food</th>
+                  <th>Item Name</th>
+                  <th>Quantity</th>
+                  <th>Price</th>
+                  <th>Action</th>
+                </tr>
+              </thead>
+              <tbody>
+                {/* row 1 */}
+                {cart.map((item, index) => (
+                  <tr key={index}>
+                    <td>{index + 1}</td>
+                    <td>
+                      <div className="flex items-center gap-3">
+                        <div className="avatar">
+                          <div className="mask mask-squircle h-12 w-12">
+                            <img
+                              src={item.image}
+                              alt="Avatar Tailwind CSS Component"
+                            />
+                          </div>
+                        </div>
+                      </div>
+                    </td>
+                    <td>{item.name}</td>
+                    <td>
+                      <button
+                        className="btn btn-xs text-red-500"
+                        onClick={() => handleDecrease(item)}
+                      >
+                        -
+                      </button>
+                      <input
+                        type="number"
+                        value={item.quantity}
+                        onChange={() => console.log(item.quantity)}
+                        className="w-10 mx-2 text-center overflow-hidden appearance-none text-[#dccf1a]"
+                      />
+                      <button
+                        className="btn btn-xs text-green-500"
+                        onClick={() => handleIncrease(item)}
+                      >
+                        +
+                      </button>
+                    </td>
+                    <td className="text-black">₹{totalPrice(item).toFixed(2)}</td>
+                    <th>
+                      <button
+                        className="btn btn-ghost btn-xs text-black hover:text-red-800 hover:bg-slate-500"
+                        onClick={() => handleDelete(item)}
+                      >
+                        <FaTrash />
+                      </button>
+                    </th>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+
+          {/* customer details */}
+          <div className="my-12 flex flex-col md:flex-row">
+            <div className="md:w-1/2 space-y-3 relative">
+              <h3 className="text-[#dccf1a] font-bold">Customer Details</h3>
+              <label
+                htmlFor="my-drawer-4"
+                className="drawer-button btn btn-ghost btn-circle avatar"
+              >
+                <div className="w-10 rounded-full">
+                  {user.photoURL ? (
+                    <img alt="User avatar" src={user.photoURL} />
+                  ) : (
+                    <img
+                      alt="Default avatar"
+                      src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg"
+                    />
+                  )}
+                </div>
+              </label>
+              <p className="absolute top-9 px-14">
+                Name: <span className="text-black">{user.displayName}</span>
+              </p>
+              <p>
+                Email: <span className="text-black">{user.email}</span>
+              </p>
+              <p>
+                User_id: <span className="text-black">{user.uid}</span>
+              </p>
+            </div>
+
+            <div className="md:w-1/2 space-y-3 flex flex-col justify-end items-end">
+              <h3 className="text-[#123a34] font-bold">Shopping Details</h3>
+              <p className="text-black">
+                Total Items:{" "}
+                <span className="text-black font-bold">{cart.length}</span>
+              </p>
+              <p className="text-black">
+                Total Price:{" "}
+                <span className="text-[#102929] font-bold">
+                  ₹{calculateTotalAmount.toFixed(2)}
+                </span>
+              </p>
+              <Link to='/process-checkout'>
+                <button className="btn bg-[#F1B24A] hover:bg-[#f0c684] hover:text-slate-800 rounded-full px-6 text-white flex items-center gap-2 mt-4 mb-5">
+                  Proceed Checkout
+                </button>
+              </Link>
+            </div>
+          </div>
+        </>
+      )}
     </div>
   );
 }
